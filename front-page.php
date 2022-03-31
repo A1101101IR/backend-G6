c58dd4e380bbbe4e04daedb172d43e5c9e8aa1c3
964Code <964code@gmail.com>
2022-03-31 13:27:21 +0200

    change overflow in post-section so X is hidden

--- front-page.php
+++ front-page.php
@@ -33,8 +33,8 @@
         <div class="tweet-text">
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
         </div>
-        <div>
-          <button class="btn-tweet edit">Edit</button>
+        <div class="btn-container-post">
+          <button class="btn-in-post edit">Edit</button>
           <button class="btn-tweet delete">Delete</button>
         </div>
       </div>
@@ -44,55 +44,12 @@
         <div class="tweet-text">
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
         </div>
-        <div>
-          <button class="btn-tweet edit">Edit</button>
+        <div class="btn-container-post">
+          <button class="btn-in-post edit">Edit</button>
           <button class="btn-tweet delete">Delete</button>
         </div>
       </div>
 
-      <div class="tweet-card-container">
-        <div class="avatar"></div>
-        <div class="tweet-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
-        </div>
-        <div>
-          <button class="btn-tweet edit">Edit</button>
-          <button class="btn-tweet delete">Delete</button>
-        </div>
-      </div>
-
-      <div class="tweet-card-container">
-        <div class="avatar"></div>
-        <div class="tweet-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
-        </div>
-        <div>
-          <button class="btn-tweet edit">Edit</button>
-          <button class="btn-tweet delete">Delete</button>
-        </div>
-      </div>
-
-      <div class="tweet-card-container">
-        <div class="avatar"></div>
-        <div class="tweet-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
-        </div>
-        <div>
-          <button class="btn-tweet edit">Edit</button>
-          <button class="btn-tweet delete">Delete</button>
-        </div>
-      </div>
-
-      <div class="tweet-card-container">
-        <div class="avatar"></div>
-        <div class="tweet-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
-        </div>
-        <div>
-          <button class="btn-tweet edit">Edit</button>
-          <button class="btn-tweet delete">Delete</button>
-        </div>
-      </div>
 
 
       <!-- End tweet card container -->
