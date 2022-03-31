ec6a752851a6cc8da28c9db9637b6f74879a6d50
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-03-31 16:31:11 +0200

    created get func, delete func, and post func (small fix needed)

--- front-page.php
+++ front-page.php
@@ -22,39 +22,15 @@
         <textarea name="tweets" id="tweet" cols="30" rows="5"></textarea>
       </div>
       <div class="btn-container">
-        <button class="tweet-btn">Chrip it/Post</button>
+        <button class="tweet-btn" onClick="postTweet()">Chrip it/Post</button>
       </div>
     </div>
 
     <div class="tweets-container">
 
-      <div class="tweet-card-container">
-        <div class="avatar"></div>
-        <div class="tweet-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
-        </div>
-        <div class="btn-container-post">
-          <button class="btn-in-post edit">Edit</button>
-          <button class="btn-tweet delete">Delete</button>
-        </div>
-      </div>
+    <div id="post"></div>
+      
 
-      <div class="tweet-card-container">
-        <div class="avatar"></div>
-        <div class="tweet-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam modi harum doloremque at corporis magnam non commodi sequi distinctio debitis, repellat nostrum accusamus nesciunt, dolores quo obcaecati delectus, pariatur unde animi recusandae sint voluptatum aspernatur? Eum quos placeat quaerat earum sequi laudantium impedit eaque nulla ratione magnam fugit tenetur ad delectus voluptatem consequuntur magni perspiciatis cum enim totam nobis tempore numquam, dolor iure aperiam. Repellat excepturi aspernatur aut magni porro ex quaerat laboriosam est minima harum sapiente quis voluptas atque necessitatibus, nemo, quam ut dolor maiores eligendi. Fugiat, odio ut eveniet ad odit ab accusantium iste omnis deleniti, cumque minus.</p>
-        </div>
-        <div class="btn-container-post">
-          <button class="btn-in-post edit">Edit</button>
-          <button class="btn-tweet delete">Delete</button>
-        </div>
-      </div>
-
-      <script>
-        let token = token;
-      </script>
-
-      <!-- End tweet card container -->
     </div>
 <!-- end content container -->
   </div>
@@ -62,7 +38,82 @@
 
 
 
+  <script>
+    /* Hämtar data coh skapar inlägg av de */
+    async function getPost() {
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      let postData = await fetch("http://localhost:8000/tweet", requestOptions);
+      let myData = await postData.json();
+      let output = "";
+      if (myData) {
+        myData.map((post) => {
+          output += `<div class="tweet-card-container" key={post.id}>  
+              <div class="avatar">
+                <img src="https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcT0BTHYsJqrUEhxjVReplkbGQlNLDzaFfKwIDXf_aiY4isJBd-3_fLVYpIWNi6r7P604hS3DRwAoyf_jnPhpAs" alt="">
+              </div>
+              <div class="tweet-text">
+                <p>${post.description}</p>
+              </div>
+              <div class="btn-container-post">
+                <button class="btn-in-post edit">Edit</button>
+                <button class="btn-tweet delete" onClick="deleteTweet(${post.id})">Delete</button>
+              </div>
+          </div>`
+        })
+      }
+      document.getElementById('post').innerHTML = output;
+    }
 
+    /* skapar nya inlägg och skickar till databasen */
+    function postTweet() {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+      input = document.getElementById('tweet').value;
+      inputData = {
+        tweet : {
+          description: input
+        }
+      }
+      console.log(inputData);
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: inputData,
+        redirect: 'follow'
+      };
+
+      fetch("http://localhost:8000/tweet/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
+
+
+
+    /* tar bort inlägg och gör en reload på sidan för att uppdatera output */
+    function deleteTweet(id) {
+      var urlencoded = new URLSearchParams();
+
+      var requestOptions = {
+        method: 'DELETE',
+        body: urlencoded,
+        redirect: 'follow'
+      };
+
+      fetch(`http://localhost:8000/tweet/${id}`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log("Tweet med id " + id + " togs bort!"))
+        .catch(error => console.log('error', error));
+        setTimeout(() => {
+          location.reload(); 
+        }, 500);
+      };
+    
+    getPost();
+  </script>
 
 
 
@@ -82,9 +133,6 @@
     
 
 </section>
-<div>
-  
-Helloo
-</div>
+
 
 <?php get_footer(); ?>
