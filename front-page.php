76c1d4525f7b2cc65fd18f03969fdb00b12d9927
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-03-31 14:26:32 +0200

    skapade db

--- front-page.php
+++ front-page.php
@@ -50,12 +50,12 @@
         </div>
       </div>
 
-
+      <script>
+        let token = token;
+      </script>
 
       <!-- End tweet card container -->
     </div>
-    
-    
 <!-- end content container -->
   </div>
 
@@ -79,24 +79,7 @@
 
 
 
-
-    <!-- loop, kollar om det finns några post att visa -->
-    <?php if (have_posts()) : while (have_posts()) : the_post();?>
-
-      <!-- kollar om post som visas har en bild, om ja visar bilden. -->
-      <?php if(has_post_thumbnail()):?>
-        <img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>">
-      <?php endif;?>
-
-      <!-- hämtar post title och visar i h3 tag -->
-      <h3><?php the_title();?></h3>
-
-      <!-- Hämtar post content (kort version av texten) och visar i en p tag -->
-      <?php the_excerpt();?>
-
-      <!-- hämtar länken till vår post och visar i en a tag -->
-      <a href="<?php the_permalink();?>"><button>Read more</button></a>
-    <?php endwhile; endif;?>
+    
 
 </section>
 <div>
